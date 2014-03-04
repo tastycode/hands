4206b3e0a0c00940a482206d68a5fe1668f168fc
Thomas Devol <pair+thomas@rally.org>
2014-03-03 16:54:19 -0800

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('handsApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
