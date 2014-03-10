b882307324b985a12e24f6bac3e2a62cf67fdc50
Thomas Devol <pair+thomas@rally.org>
2014-03-09 20:01:36 -0700

    Initial homepage design

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
 angular.module('handsApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $location) {
+    $scope.go = function(path) {
+      $location.path(path);
+    };
   });
